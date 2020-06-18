bdd8f2b8895e4fa9b1c05dae41cde591ec7f2e82
jakiiii <me.jaki@outlook.com>
2020-06-18 20:49:48 +0600

    update courses info

--- LMS/instructor/forms.py
+++ LMS/instructor/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.utils import timezone
 
+from courses.models import Courses, Lessons, Section, Topic
 from blog.models import Blog
 
 
@@ -20,8 +21,8 @@ class CreateBlogPostFrom(forms.ModelForm):
             'content',
             'tags',
             'active',
-            'timestamp',
-            'slug',
+            # 'timestamp',
+            # 'slug',
         ]
         widgets = {
             'timestamp': DateTimeInput(),
@@ -34,6 +35,43 @@ class CreateBlogPostFrom(forms.ModelForm):
             'content': 'Content',
             'tags': 'Tags',
             'active': 'Active',
-            'timestamp': 'Timestamp',
-            'slug': 'Slug',
+            # 'timestamp': 'Timestamp',
+            # 'slug': 'Slug',
+        }
+
+
+class CreateCourseForm(forms.ModelForm):
+
+    class Meta:
+        model = Courses
+        fields = [
+            'title',
+            'course_short_description',
+            'course_long_description',
+            'will_learn',
+            'course_image',
+            'course_intro_video',
+            'course_complete_duration',
+            'course_for',
+            'category',
+            'subcategory',
+            'is_active',
+            'is_featured',
+            'is_featured',
+            'is_locked',
+        ]
+        labels = {
+            'title': 'Title',
+            'course_short_description': 'Course Short Description',
+            'course_long_description': 'Course Long Description',
+            'will_learn': 'What you\'ll learn',
+            'course_image': 'Course Image',
+            'course_intro_video': 'Course Intro Video',
+            'course_complete_duration': 'Course Complete Duration',
+            'course_for': 'Course For',
+            'category': 'Category',
+            'subcategory': 'Subcategory',
+            'is_active': 'Is Active',
+            'is_featured': 'Is Featured',
+            'is_locked': 'Is Locked',
         }
